fe44608ce407cbed1ed3da05ca6e4b74f49d307e
Philipp <philipp.trilk@live.de>
2026-04-06 21:18:09 +0200

    some changes trhough Codex

---
+++ src/components/contactData.ts
@@ -0,0 +1,12 @@
+export const contactDetails = {
+    name: "Moritz Zimmer",
+    addressLine1: "Lindenstraße 7",
+    addressLine2: "67822 Finkenbach-Gersweiler",
+    instagramUrl: "https://www.instagram.com/moritz.zimmer_gartengestaltung/",
+    instagramHandle: "@moritz.zimmer_gartengestaltung",
+    serviceArea: "Finkenbach-Gersweiler und Umgebung",
+    phone: "+4915150055155",
+    email: "moritz.zimmer-gartengestaltung@magenta.de",
+    mapsEmbedUrl:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2581.6220241598053!2d7.744120176927023!3d49.68025814251888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47960387ccc7e787%3A0xfc603a5b092e247f!2sLindenstra%C3%9Fe%207%2C%2067822%20Finkenbach-Gersweiler!5e0!3m2!1sde!2sde!4v1743080052454!5m2!1sde!2sde",
+} as const;
